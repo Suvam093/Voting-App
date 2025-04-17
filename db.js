c662d2f9714d6824d049f728e1af44ac89715853
Suvam093 <suvamshaw86@gmail.com>
2025-04-17 13:10:54 +0530

    updated the db file to be compatible for deployment

--- db.js
+++ db.js
@@ -1,27 +1,16 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 // const mongoURL = process.env.MONGODB_LOCAL_URL;
 const mongoURL = process.env.MONGODB_URI;
 console.log(mongoURL);
 
-mongoose.connect(mongoURL, {
-    tlsAllowInvalidCertificates: true, // Ignore SSL certificate issues
-    serverSelectionTimeoutMS: 5000 // Prevents infinite retry loops
-  })
-
-const db = mongoose.connection;
-
-db.on('connected', () => {
-    console.log("Connected to MongoDB");
-})
-
-db.on('error', (error) => {
-    console.log("Error connecting to MongoDB", error);
-})
-
-db.on('disconnected', () => {
-    console.log("Disconnected from MongoDB");
-})
+const db = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);   
+    }   
+}
 
 module.exports = db; 
